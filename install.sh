88475c4e2116e33b3c656e0f38e2b5534b028d0b
gegeriyadi <gegeriyadi81@gmail.com>
2020-06-11 16:22:51 +0700

    new install script

--- install.sh
+++ install.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-git clone https://github.com/gegeriyadi/dotfiles
-mv dotfiles .dotfiles
-cd .dotfiles
+
+mkdir -p ~/.dotfiles
+cp * ~/.dotfiles/
+
 ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/gitignore ~/.gitignore
 ln -s ~/.dotfiles/bashrc ~/.bashrc
 ln -s ~/.dotfiles/profile ~/.profile
+mkdir -p ~/.vim/after/ftplugin
+ln -s ~/.dotfiles/vim/after/ftplugin/html.vim ~/.vim/after/ftplugin/html.vim
+ln -s ~/.dotfiles/vim/after/ftplugin/php.vim ~/.vim/after/ftplugin/php.vim
+ln -s ~/.dotfiles/vim/after/ftplugin/vue.vim ~/.vim/after/ftplugin/vue.vim
+ln -s ~/.dotfiles/vim/after/ftplugin/css.vim ~/.vim/after/ftplugin/css.vim
+ln -s ~/.dotfiles/vim/after/ftplugin/js.vim ~/.vim/after/ftplugin/js.vim
+
 echo "dotfile installed!"
