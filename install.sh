46ed1972b7d3c81ae12c78930046a9ff048bb54a
gegeriyadi <gegeriyadi81@gmail.com>
2020-11-18 12:32:07 +0700

    install script with git clone and rename folder

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git clone git@github.com:gegeriyadi/dotfiles.git ~/.dotfiles
-
+git clone git@github.com:gegeriyadi/dotfiles.git
+mv dotfiles .dotfiles
 ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
